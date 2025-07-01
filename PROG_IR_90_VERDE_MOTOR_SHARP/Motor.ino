8d569e2f6ec64690a1ff8063fec9f84d4a276846
Muryllo S. Teixeira <162444926+Muryllost@users.noreply.github.com>
2025-07-01 16:01:25 -0300

    Update Motor.ino

--- PROG_IR_90_VERDE_MOTOR_SHARP/Motor.ino
+++ PROG_IR_90_VERDE_MOTOR_SHARP/Motor.ino
@@ -1,5 +1,3 @@
-
-
 // Motor A = Esquerdo (IN1, IN2, PWM_A)
 // Motor B = Direito  (IN3, IN4, PWM_B)
 
@@ -8,6 +6,7 @@ void Motor(){
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
+
 }
 
 void PararMotor(){  
@@ -20,6 +19,8 @@ void PararMotor(){
 void MotorRe() {
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
 }
@@ -43,9 +44,13 @@ void MotorEsq90(){
 //=========================================================//
 void MoverFrente() {//LINHA RETA
   Motor();  
+  analogWrite(PWM_A, vel_A  );//LINHA RETA
+  analogWrite(PWM_B, vel_B  );//LINHA RETA
   analogWrite(PWM_A, vel_A + 20 );//LINHA RETA
   analogWrite(PWM_B, vel_B + 20 );//LINHA RETA
 }
+
+void MoverAcenDir(){//CURVA ACENTUADA A DIREITA
 //=========================================================//
 //--- CURVA ACENTUADA A DIREITA ---
 void MoverAcenDir(){
@@ -53,16 +58,21 @@ void MoverAcenDir(){
   analogWrite(PWM_A, vel_A + 120);//CURVA ACENTUADA A DIREITA
   analogWrite(PWM_B, vel_B + 120);//CURVA ACENTUADA A DIREITA
 }
+
+void MoverAcenEsq(){//CURVA ACENTUADA A ESQUERDA
 //--- CURVA ACENTUADA A ESQUERDA ---
 void MoverAcenEsq(){
   MotorEsq90(); 
   analogWrite(PWM_A, vel_A + 120);//CURVA ACENTUADA A ESQUERDA
   analogWrite(PWM_B, vel_B + 120);//CURVA ACENTUADA A ESQUERDA
 }
+
 //=========================================================//
 // --- Curva Direita 90 ---
 void CurvaDir90(){
   MotorDir90();
+  analogWrite(PWM_A, vel_A + 150);
+  analogWrite(PWM_B, vel_B + 150);
   analogWrite(PWM_A, vel_A + 120);
   analogWrite(PWM_B, vel_B + 120);
 }
@@ -70,49 +80,55 @@ void CurvaDir90(){
 // --- Curvs Esquerda 90 ---
 void CurvaEsq90(){
   MotorEsq90();
+  analogWrite(PWM_A, vel_A + 150);
+  analogWrite(PWM_B, vel_B + 150);
   analogWrite(PWM_A, vel_A + 120);
   analogWrite(PWM_B, vel_B + 120);
 }
 
+// void Voltar(){
+//   MotorRe();
+//   analogWrite(PWM_A, vel_A  );//LINHA RETA
+//   analogWrite(PWM_B, vel_B  );//LINHA RETA
+// }
 //=========================================================//
 // --- VERDE Direita ---
 void CurvaDir90Ved(){
   MotorDir90();
   analogWrite(PWM_A, vel_A + 120);
-  analogWrite(PWM_B, vel_B + 120);
+  analogWrite(PWM_B, vel_B + 140);
 
-  executandoManobra = true;
+    executandoManobra = true;
   tempoFinalManobra = millis() + 700;
 }
-
 // --- VERDE Esquerda ---
 void CurvaEsq90Ved(){
   MotorEsq90();
-  analogWrite(PWM_A, vel_A + 120);
+  analogWrite(PWM_A, vel_A + 140);
   analogWrite(PWM_B, vel_B + 120);
 
-  executandoManobra = true;
+    executandoManobra = true;
   tempoFinalManobra = millis() + 1200; // duração real da curva
 }
-
-//=========================================================//
-// --- Curva Direita 90 (Sharp) ---
-void CurvaDir90Sharp(){
-  MotorDir90();
-  analogWrite(PWM_A, vel_A + 120);
-  analogWrite(PWM_B, vel_B + 120);
-}
-
-// --- Curva Esquerda 90 (Sharp) ---
-void CurvaEsq90Sharp(){
-  MotorEsq90();
-  analogWrite(PWM_A, vel_A + 120);
-  analogWrite(PWM_B, vel_B + 120);
-}
-
 //=========================================================//
 void Voltar(){
   MotorRe();
-  analogWrite(PWM_A, vel_A);
-  analogWrite(PWM_B, vel_B);
+  analogWrite(PWM_A, vel_A  );//LINHA RETA
+  analogWrite(PWM_B, vel_B  );//LINHA RETA
 }
+//=========================================================//
+
+// --- Curva Direita 90 ---
+void CurvaDir90Sharp(){
+  MotorDir90();
+  analogWrite(PWM_A, vel_A + 140);
+  analogWrite(PWM_B, vel_B + 160);
+}
+
+// --- Curvs Esquerda 90 ---
+void CurvaEsq90Sharp(){
+  MotorEsq90();
+  analogWrite(PWM_A, vel_A + 140);
+  analogWrite(PWM_B, vel_B + 160);
+}
+//=========================================================//
