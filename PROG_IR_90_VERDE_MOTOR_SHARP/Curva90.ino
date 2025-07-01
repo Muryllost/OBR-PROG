07397386656966190e81f1fef74b9e7c03fbdecf
Muryllo S. Teixeira <162444926+Muryllost@users.noreply.github.com>
2025-07-01 16:00:34 -0300

    Update Curva90.ino

--- PROG_IR_90_VERDE_MOTOR_SHARP/Curva90.ino
+++ PROG_IR_90_VERDE_MOTOR_SHARP/Curva90.ino
@@ -1,10 +1,13 @@
-bool Curva90() {
+bool Curva90() {More actions
   bool s0 = digitalRead(IR[0]);
   bool s1 = digitalRead(IR[1]);
   bool s2 = digitalRead(IR[2]);
   bool s3 = digitalRead(IR[3]);
   bool s4 = digitalRead(IR[4]);
 
+  // --- CURVA 90 DIREITA ---
+   if (((s4 == 0) && (s3 == 0) && (s2 == 0) && (s1 == 0) && (s0 == 1)) || ((s4 == 0) && (s3 == 0) && (s2 == 0) && (s1 == 1) && (s0 == 1))) {
+   Serial.println("Curva Direita");
   // Curva Direita
   if (((s4 == 0) && (s3 == 0) && (s2 == 0) && (s1 == 0) && (s0 == 1)) ||
       ((s4 == 0) && (s3 == 0) && (s2 == 0) && (s1 == 1) && (s0 == 1))) {
@@ -13,6 +16,9 @@ bool Curva90() {
     return true;
   }
 
+  // --- CURVA 90 ESQUERDA ---
+  else if (((s4 == 1) && (s3 == 0) && (s2 == 0) && (s1 == 0) && (s0 == 0)) || ((s4 == 1) && (s3 == 1) && (s2 == 0) && (s1 == 0) && (s0 == 0))) {
+   Serial.println("Curva Esquerda");
   // Curva Esquerda
   if (((s4 == 1) && (s3 == 0) && (s2 == 0) && (s1 == 0) && (s0 == 0)) ||
       ((s4 == 1) && (s3 == 1) && (s2 == 0) && (s1 == 0) && (s0 == 0))) {
@@ -21,5 +27,5 @@ bool Curva90() {
     return true;
   }
 
-  return false;
+  return false;More actions
 }
